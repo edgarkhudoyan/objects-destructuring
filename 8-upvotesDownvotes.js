fbfa2ccc9dbb9c8a2062b31d01c6e9b516328578
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 02:18:30 +0400

    feat: add getVoteCount function and declare obj1

--- 8-upvotesDownvotes.js
+++ 8-upvotesDownvotes.js
@@ -1,2 +1,14 @@
 // Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
 'use strict';
+function getVoteCount(obj) {
+  let obj1 = {
+    upvotes: obj.upvotes,
+    downvotes: obj.downvotes,
+  };
+
+  return obj1.upvotes - obj1.downvotes;
+}
+
+getVoteCount({ upvotes: 13, downvotes: 0 }); // ➞ 13
+getVoteCount({ upvotes: 2, downvotes: 33 }); // ➞ -31
+getVoteCount({ upvotes: 132, downvotes: 132 }); // ➞ 0
