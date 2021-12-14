2fc5360f73c5c3c85654d647025ae44480563c7b
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 01:38:57 +0400

    feat: add description

---
+++ whatOutput.js
@@ -0,0 +1,6 @@
+// What is the output after run
+
+// let person = { name: "Sarah", country: "Nigeria", job: "Developer" };
+// let name, country, job;
+// const { name, country, job } = person;
+// console.log(name);
