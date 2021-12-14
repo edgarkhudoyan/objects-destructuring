558141504a0cae2c5269e7c8585534dcb2889776
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 02:09:46 +0400

    feat: add task description

---
+++ 7-getUsingDestructurizing.js
@@ -0,0 +1,6 @@
+// Get name, country and job using destructuring
+'use strict';
+let person = { name: 'Sarah', country: 'Armenia', job: 'Developer' };
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
