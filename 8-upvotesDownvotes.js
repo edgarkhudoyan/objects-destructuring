0d252ef9b1e684755cd53d86549fe2d1730b74d5
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 02:14:16 +0400

    feat: add task description

---
+++ 8-upvotesDownvotes.js
@@ -0,0 +1,2 @@
+// Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+'use strict';
