af280067c0b6f0e44fb57cb2627b93a86b44c7c5
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 01:41:48 +0400

    feat: add solution using destructurizing

--- getNameCountryJob.js
+++ getNameCountryJob.js
@@ -1,2 +1,10 @@
 // Get name, country and jod using destructuring
 'use strict';
+
+let person = { name: 'Sarah', country: 'Armenia', job: 'Developer' };
+
+let { name, country, job } = person;
+
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
