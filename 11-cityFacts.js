22ef23376afeaf578917d1b1ede2dca9674ac115
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 12:09:06 +0400

    feat: declare cityFacts function

--- 11-cityFacts.js
+++ 11-cityFacts.js
@@ -1,2 +1,15 @@
 // The string should have the following format: X has a population of Y and is situated in Z (where X is the city name, Y is the population and Z is the continent the city is situated in).
 'use strict';
+
+function cityFacts(city) {
+  let { name, population, continent } = city;
+  return `${name} has a population of ${population} and is situated in ${continent}`;
+}
+
+console.log(
+  cityFacts({
+    name: 'Paris',
+    population: '2,140,526',
+    continent: 'Europe',
+  })
+);
