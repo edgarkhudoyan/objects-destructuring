2c44ca114486526a75668b23669186323b497949
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 11:54:05 +0400

    feat: add task11

---
+++ 11-cityFacts.js
@@ -0,0 +1,2 @@
+// The string should have the following format: X has a population of Y and is situated in Z (where X is the city name, Y is the population and Z is the continent the city is situated in).
+'use strict';
