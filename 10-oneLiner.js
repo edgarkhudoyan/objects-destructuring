c32365a5a2324288db92991e580d22694b6b762a
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 11:49:17 +0400

    feat: add showCityDetails function

--- 10-oneLiner.js
+++ 10-oneLiner.js
@@ -1,2 +1,14 @@
 // Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
 'use strict';
+
+let city = {
+  name: 'Yerevan',
+  pop: '1M',
+  continent: 'Asia',
+};
+
+function showCityDetails(obj) {
+  return Object.values(obj);
+}
+
+console.log(showCityDetails(city));
