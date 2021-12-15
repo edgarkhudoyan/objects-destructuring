8a4fbf4ae391a1b4e6ec537893c3faeffbce5f21
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-15 13:23:24 +0400

    feat: add key values and entries methods

--- 13-methods.js
+++ 13-methods.js
@@ -1,7 +1,24 @@
 // Write object.keys, object.values and object.entries methods using for..in
-let obj = {
+const obj = {
   name: 'John',
   age: 30,
   job: 'Developer',
   isAdmin: true,
 };
+
+// Object.keys
+for (let property in obj) {
+  console.log(Object.keys(obj));
+}
+
+// Object.values
+for (let values in obj) {
+  console.log(Object.values(obj));
+}
+
+//Object.entries
+for (let entries in obj) {
+  for (let [key, value] of Object.entries(obj)) {
+    console.log(`${key}: ${value}`);
+  }
+}
