ebdb828e090085a064174704a16ab2f24cd6c962
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-18 22:33:13 +0400

    fix: add nums rest param and use reduce method

--- 12-printSum.js
+++ 12-printSum.js
@@ -1,13 +1,8 @@
 // Create a function that takes infinite count of elements, operator and prints their sum. If there's no operator, then default should be +
 'use strict';
 
-function allSum() {
-  let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
-
-  return sum;
+function allSum(...nums) {
+  return nums.reduce((total, cur) => total + cur);
 }
 
 console.log(allSum(5, 6, 7, 10, 4));
