f8831ae878fa1ef237f9c56954f747ddab278531
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-18 22:31:07 +0400

    feat: declare allSum function

--- 12-printSum.js
+++ 12-printSum.js
@@ -1,2 +1,13 @@
 // Create a function that takes infinite count of elements, operator and prints their sum. If there's no operator, then default should be +
 'use strict';
+
+function allSum() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+
+  return sum;
+}
+
+console.log(allSum(5, 6, 7, 10, 4));
